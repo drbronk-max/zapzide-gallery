c7bced22360262ebc7f5f10cdfe563444ae1a68f
drbronk-max <144430729+drbronk-max@users.noreply.github.com>
2026-07-03 10:04:03 +0200

    Update nostr.ts

--- src/nostr.ts
+++ src/nostr.ts
@@ -1,7 +1,7 @@
 import { EventStore, mapEventsToStore } from "applesauce-core";
 import { RelayPool } from "applesauce-relay";
 import { BehaviorSubject } from "rxjs";
-import { LIMIT } from "./config";
+import { LIMIT, FILTER_TERM } from "./config";
 
 export const eventStore = new EventStore();
 export const loading$ = new BehaviorSubject(true);
@@ -15,8 +15,15 @@ export function loadGm(pubkey: string, relays: string[]) {
   if (started) return;
   started = true;
 
+  const filters = FILTER_TERM
+    ? [
+        { kinds: [0], authors: [pubkey], limit: 1 },
+        { kinds: [1], authors: [pubkey], "#t": [FILTER_TERM], limit: LIMIT },
+      ]
+    : [{ kinds: [0, 1], authors: [pubkey], limit: LIMIT }];
+
   pool
-    .request(relays, { kinds: [0, 1], authors: [pubkey], limit: LIMIT })
+    .request(relays, filters)
     .pipe(mapEventsToStore(eventStore))
     .subscribe({
       complete: () => loading$.next(false),
@@ -29,7 +36,9 @@ export function loadGm(pubkey: string, relays: string[]) {
 
 /** Fetch an older page of a user's notes (before `until`) into the store. */
 export function loadMore(pubkey: string, relays: string[], until: number) {
-  return pool
-    .request(relays, { kinds: [1], authors: [pubkey], until, limit: LIMIT })
-    .pipe(mapEventsToStore(eventStore));
+  const filter = FILTER_TERM
+    ? { kinds: [1], authors: [pubkey], "#t": [FILTER_TERM], until, limit: LIMIT }
+    : { kinds: [1], authors: [pubkey], until, limit: LIMIT };
+
+  return pool.request(relays, filter).pipe(mapEventsToStore(eventStore));
 }
